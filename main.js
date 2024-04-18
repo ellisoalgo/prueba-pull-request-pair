57017d7f26e4cd518ce4ee97a0860b922467c3e3
ellisoalgo <ellisjwhitney@gmail.com>
2024-04-18 11:12:26 +0200

    winky face click and hover

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 "use strict";
 
+const face = document.querySelector(".js-face");
+
+function winkyFace(){
+    face.innerHTML = ";)";
+}
+
+function handleHover(){
+    face.innerHTML = ":)";
+};
+
+face.addEventListener('click', winkyFace);
+
+face.addEventListener('mouseout', handleHover);
